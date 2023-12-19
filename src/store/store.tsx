5e02123b5449a96f1d5d6ce335be147a3b5af4a3
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-19 23:52:14 +0400

    feat:add zustand store

---
+++ src/store/store.tsx
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+interface DarkModeState {
+  darkMode: boolean;
+  changeMode: (by: boolean) => void;
+}
+const checkLocalStorage =
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches);
+export const useThemeStore = create<DarkModeState>()((set: any) => ({
+  darkMode: checkLocalStorage,
+  changeMode: (value: boolean) => set({ darkMode: value }),
+}));

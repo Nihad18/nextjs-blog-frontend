5e6b2a78008ef10e18f3e0a67b9dc5bca48f67f3
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-22 23:47:22 +0400

    fix: changed useEffect dependency array value changeMode to darkMode , and dark mode worked

--- src/utils/hooks/useLocalStorage.tsx
+++ src/utils/hooks/useLocalStorage.tsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useThemeStore } from "@/store/store.tsx";
 export function useLocalStorage() {
+  const darkMode = useThemeStore((state:any)=>state.darkMode)
   const changeMode = useThemeStore((state: any) => state.changeMode);
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
   useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-
-    if (storedTheme === "dark" || (!storedTheme && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    const storedTheme = darkMode ? "dark" : "light";
+    if (
+      storedTheme === "dark") {
+      document.documentElement.classList.add("dark");
       changeMode(true); // dark mode
     } else {
+      document.documentElement.classList.remove("dark");
       changeMode(false); // light mode
     }
-  }, [changeMode]);
+  }, [darkMode]);
 }

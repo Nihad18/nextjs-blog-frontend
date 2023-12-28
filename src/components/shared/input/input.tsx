2e598c987c91f81a5b6e0f6f0de38a9a415e558a
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-28 23:14:44 +0400

    feat(component): changed color class names

--- src/components/shared/input/input.tsx
+++ src/components/shared/input/input.tsx
@@ -11,11 +11,11 @@ const Input = (props: any) => {
       </label>
       <Field
         {...props}
-        className='bg-gray-50 border border-gray-300 
-        text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600
-         focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 
-         dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-         dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        className='bg-lite-gray border border-lite-gray 
+        text-dark-gray sm:text-sm rounded-lg focus:ring-blue
+         focus:border-blue block w-full p-2.5 dark:bg-gray 
+         dark:border-gray dark:placeholder-lite-gray dark:text-white 
+         dark:focus:ring-blue dark:focus:border-blue'
         required={true}
       />
     </div>

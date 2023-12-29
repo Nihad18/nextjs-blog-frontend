8ad3053cac2ffb9411eb82118e1d8f3522cb491f
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-29 23:58:37 +0400

    feat(interface): created interface for auth forms

---
+++ src/utils/interfaces/auth.tsx
@@ -0,0 +1,10 @@
+export interface IRegisterForm {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+export interface ILoginForm {
+  email: string;
+  password: string;
+}

046d4d8e84e369e47bea1d51040b9a7fa3e84392
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-31 23:35:58 +0400

    created activate account page

---
+++ src/app/(user)/activate-account/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ActivateAccount from '@/components/auth/activate-account/activate-account'
+const ActivateAccountPage = () => {
+  return (
+    <>
+    <ActivateAccount/>
+    </>
+  )
+}
+
+export default ActivateAccountPage

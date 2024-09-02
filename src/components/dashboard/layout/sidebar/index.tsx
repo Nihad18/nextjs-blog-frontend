c412c05d03b2eb0b10d085c4ee41b916b171cb4a
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-09-02 22:16:11 +0400

    refactor: removed dividers

--- src/components/dashboard/layout/sidebar/index.tsx
+++ src/components/dashboard/layout/sidebar/index.tsx
@@ -6,7 +6,6 @@ import { IoClose } from "react-icons/io5";
 import "./style.css";
 //zustand
 import { adminStore } from "@/store/admin";
-import { Divider } from "antd";
 function Sidebar() {
   const toggle = adminStore((state: any) => state.toggle);
   const setToggle = adminStore((state: any) => state.setToggle);
@@ -21,30 +20,26 @@ function Sidebar() {
           Home
         </Link>
 
-        <Divider className='bg-gray min-w-[100px] w-[100px] my-3' />
-
         <Link href='/admin' className='link' onClick={() => setToggle(false)}>
           Admin
         </Link>
 
-        <Divider className='bg-gray min-w-[100px] w-[100px] my-3' />
-
-        <Link href='/admin/tags' className='link' onClick={() => setToggle(false)}>
+        <Link
+          href='/admin/tags'
+          className='link'
+          onClick={() => setToggle(false)}
+        >
           Tags
         </Link>
-
-        <Divider className='bg-gray min-w-[100px] w-[100px] my-3' />
 
         <Link href='/' className='link' onClick={() => setToggle(false)}>
           Blogs
         </Link>
-        <Divider className='bg-gray min-w-[100px] w-[100px] my-3' />
 
         <Link href='/login' className='link' onClick={() => setToggle(false)}>
           Login
         </Link>
 
-        <Divider className='bg-gray min-w-[100px] w-[100px] my-3' />
         {toggle && (
           <IoClose className='close-button' onClick={() => setToggle(false)} />
         )}

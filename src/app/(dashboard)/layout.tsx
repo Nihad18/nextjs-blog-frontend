2ba8c4e227774563857b2ee0c24e5e614c6b85be
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-09-08 00:26:42 +0400

    feat:imported admin.css file

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,15 @@
+//next
 import type { Metadata } from "next";
-//style files
-import "@/assets/styles/globals.css";
 //components
 import { ThemeProviders } from "../theme-provider";
 import Sidebar from "@/components/dashboard/layout/sidebar";
 import Navbar from "@/components/dashboard/layout/navbar";
 //fonts
 import { Inter } from 'next/font/google'
- 
+//style files
+import "@/assets/styles/globals.css";
+import "@/assets/styles/admin.css";
+
 export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
@@ -25,7 +27,7 @@ export default function AdminLayout({
   return (
     <html lang='en'>
       <body
-        className={`${inter.className} text-black dark:text-white bg-light dark:bg-dark transition-colors`}
+        className={`${inter.className} text-black dark:text-white bg-white dark:bg-dark transition-colors`}
       >
         <ThemeProviders>
           <div className='flex w-screen min-h-screen'>

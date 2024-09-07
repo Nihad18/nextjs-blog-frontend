394c0bb98566878c0781e309e1accf65719328aa
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-09-08 00:33:08 +0400

    feat:add custom dynamic breadcrumb

--- src/app/(dashboard)/admin/tags/create/page.tsx
+++ src/app/(dashboard)/admin/tags/create/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import Link from "next/link";
-import "@/assets/styles/admin.css";
-import { HomeOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button, Divider } from "antd";
-
+//antd
+import { Button, Divider } from "antd";
+//formik
 import { Formik, Form, FormikHelpers, FormikErrors } from "formik";
 import { toFormikValidationSchema } from "zod-formik-adapter";
+//utils
 import { ITagForm } from "@/utils/interfaces/admin";
 import { tagFormSchema } from "@/utils/schemas/admin";
+//components
 import Input from "@/components/shared/input/input";
+import BreadCrumb from "@/components/shared/breadcrumb";
 
 export default function Tags() {
   const handleSubmit = async (
@@ -17,39 +18,14 @@ export default function Tags() {
     { setSubmitting, resetForm }: FormikHelpers<ITagForm>
   ) => {
     console.log(values);
-    // const { loading, error, data } = await LoginService(values);
-    // if (error) {
-    //   console.log(error);
-    // } else if (loading) {
-    //   console.log(loading);
-    // }
-    // console.log(data);
     setSubmitting(false);
     resetForm();
   };
   return (
     <main className='page'>
-      <Breadcrumb
-        className='breadcrumb'
-        items={[
-          {
-            title: (
-              <>
-                <HomeOutlined />
-                <Link href='/admin'>Admin</Link>
-              </>
-            ),
-          },
-          {
-            title: <Link href='/admin/tags'>Tags</Link>,
-          },
-          {
-            title: <span className=''>Create tag</span>,
-          },
-        ]}
-      />
-      <Divider className='bg-gray my-3' />
-      <h1 className="head-text">Create tag</h1>
+      <BreadCrumb />
+      <Divider className='dark:bg-white my-3' />
+      <h1 className='head-text'>Create tag</h1>
       <Formik
         initialValues={{ tagName: "" }}
         onSubmit={handleSubmit}
@@ -71,16 +47,10 @@ export default function Tags() {
               placeholder='Tag name'
             />
             <div className='text-red'>{errors.tagName}</div>
-            {/* <button
-              className='submit-button'
-              type='submit'
-              disabled={isSubmitting}
-            >
-              Submit
-            </button> */}
             <Button
               type='primary'
-              htmlType="submit"
+              className='dark:bg-violet-700 dark:hover:!bg-violet-600'
+              htmlType='submit'
               disabled={isSubmitting}
               size='large'
             >

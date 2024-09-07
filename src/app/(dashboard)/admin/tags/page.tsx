732ddded6134f63df22d17ae9730046e8034e918
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-09-08 00:32:11 +0400

    feat:add custom dynamic breadcrumb

--- src/app/(dashboard)/admin/tags/page.tsx
+++ src/app/(dashboard)/admin/tags/page.tsx
@@ -1,29 +1,16 @@
 "use client";
-
-import "@/assets/styles/admin.css";
-import { HomeOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button, Divider } from "antd";
+//next
 import Link from "next/link";
+//antd
+import {Button, Divider } from "antd";
+// components
+import BreadCrumb from "@/components/shared/breadcrumb";
+
 export default function Tags() {
   return (
     <main className='page'>
-      <Breadcrumb
-        className='breadcrumb'
-        items={[
-          {
-            title: (
-              <>
-                <HomeOutlined />
-                <Link href='/admin'>Admin</Link>
-              </>
-            ),
-          },
-          {
-            title: <span>Tags</span>,
-          },
-        ]}
-      />
-      <Divider className='bg-gray my-3' />
+      <BreadCrumb />
+      <Divider className='dark:bg-white my-3' />
       <div className='header'>
         <h1 className='text-2xl'>Tags</h1>
         <Link href='/admin/tags/create'>
